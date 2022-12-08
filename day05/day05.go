bd60b4e5d83b4fa1dfbd813548d97e7d29088207
yurket <yurket2006@yandex.ru>
2022-12-07 18:52:11 -0800

    Day05

--- day05/day05.go
+++ day05/day05.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-type Crates map[int][]rune
+type Stacks map[int][]rune
 type Instructions []Instruction
 type Instruction struct {
 	count int
@@ -21,43 +21,48 @@ func reverseSlice[T comparable](s []T) {
 	})
 }
 
-func reverseCrates(crates *Crates) {
-	for _, crate := range *crates {
-		reverseSlice(crate)
+func reverseStacks(stacks *Stacks) {
+	for _, stack := range *stacks {
+		reverseSlice(stack)
 	}
 }
 
-func parseCrates(cratesS string) Crates {
-	lines := strings.Split(cratesS, "\n")
+func printStacks(stacks Stacks) {
+	for i := 1; i <= len(stacks); i++ {
+		for _, c := range stacks[i] {
+			fmt.Printf("'%s' ", string(c))
+		}
+		fmt.Println()
+	}
+
+}
+
+func parseStacks(stacksS string) Stacks {
+	lines := strings.Split(stacksS, "\n")
 	for lines[len(lines)-1] == "" {
 		lines = lines[:len(lines)-1]
 	}
 	// drop line with crates numbers
 	lines = lines[:len(lines)-1]
 
-	crates := Crates{}
-	crateNum := 1
+	stacks := Stacks{}
+	stackNum := 1
 	for _, line := range lines {
-		for _, maybeCrates := range strings.Split(line, "    ") {
-			if maybeCrates == "" {
-				crateNum++
+		for i := 0; i < len(line); i += 4 {
+			var crate rune
+			_, err := fmt.Sscanf(line[i:i+3], "[%c]", &crate)
+			if err == nil {
+				stacks[stackNum] = append(stacks[stackNum], crate)
 			}
-
-			for _, s := range strings.Split(maybeCrates, "[") {
-				if s == "" {
-					continue
-				}
-				crateLetter := s[0]
-				crates[crateNum] = append(crates[crateNum], rune(crateLetter))
-				crateNum++
-			}
+			stackNum++
 		}
-		crateNum = 1
+		stackNum = 1
 	}
 
-	reverseCrates(&crates)
-	fmt.Println(crates)
-	return crates
+	reverseStacks(&stacks)
+
+	printStacks(stacks)
+	return stacks
 }
 
 func parseInstructions(instr string) Instructions {
@@ -79,7 +84,7 @@ func parseInstructions(instr string) Instructions {
 	return instructions
 }
 
-func parseInput(filename string) (Crates, Instructions) {
+func parseInput(filename string) (Stacks, Instructions) {
 	content, err := os.ReadFile(filename)
 	if err != nil {
 		panic(err)
@@ -89,18 +94,68 @@ func parseInput(filename string) (Crates, Instructions) {
 	if len(twoParts) != 2 {
 		panic("Can't distinguish crates from instructions!")
 	}
-	crates, instructions := twoParts[0], twoParts[1]
-	return parseCrates(crates), parseInstructions(instructions)
+	stacks, instructions := twoParts[0], twoParts[1]
+	return parseStacks(stacks), parseInstructions(instructions)
 }
 
-func solve(filename string) int {
-	// crates, instructions := parseInput(filename)
+func moveCrates(stacks Stacks, instructions Instructions) {
+	for _, instr := range instructions {
+		fromStack, toStack := stacks[instr.from], stacks[instr.to]
 
-	res := 0
-	fmt.Printf("[Part1] Total pairs: %d\n", res)
-	return res
+		for i := instr.count; i != 0; i-- {
+			crate := fromStack[len(fromStack)-1]
+			fromStack = fromStack[:len(fromStack)-1]
+			toStack = append(toStack, crate)
+		}
+		stacks[instr.from] = fromStack
+		stacks[instr.to] = toStack
+	}
+}
+
+func moveCrates9001(stacks Stacks, instructions Instructions) {
+	for _, instr := range instructions {
+		fromStack, toStack := stacks[instr.from], stacks[instr.to]
+
+		s, e := len(fromStack)-instr.count, len(fromStack)
+		crates := fromStack[s:e]
+		fromStack = fromStack[:s]
+		toStack = append(toStack, crates...)
+
+		stacks[instr.from] = fromStack
+		stacks[instr.to] = toStack
+	}
+}
+
+func getTopCrates(stacks Stacks) string {
+	var topCrates string
+	for i := 1; i <= len(stacks); i++ {
+		topCrates += string(stacks[i][len(stacks[i])-1])
+	}
+
+	return topCrates
+}
+
+func part1(filename string) {
+	topCrates := solve(filename, moveCrates)
+	fmt.Printf("[Part1] Total pairs: %s\n", topCrates)
+}
+
+func part2(filename string) {
+	topCrates := solve(filename, moveCrates9001)
+	fmt.Printf("[Part2] Total pairs: %s\n", topCrates)
+}
+
+func solve(filename string, movingFunction func(Stacks, Instructions)) string {
+	stacks, instructions := parseInput(filename)
+
+	movingFunction(stacks, instructions)
+	fmt.Println("\nStack after moving crates:")
+	printStacks(stacks)
+
+	return getTopCrates(stacks)
 }
 
 func main() {
-	solve("input")
+	part1("input")
+	part2("input")
 }
--- day05/day05_test.go
+++ day05/day05_test.go
@@ -6,11 +6,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// func TestPart1(t *testing.T) {
-// 	pairs, _ := solve("my_test_input")
+func TestPart1(t *testing.T) {
+	topCrates := solve("my_test_input", moveCrates)
 
-// 	require.Equal(t, 2, pairs)
-// }
+	require.Equal(t, "CMZ", topCrates)
+}
 
 func TestParseInstructions(t *testing.T) {
 	s := `move 3 from 1 to 3
@@ -23,18 +23,35 @@ move 2 from 2 to 1`
 	require.Equal(t, Instruction{2, 2, 1}, instructions[1])
 }
 
-func TestParseCrates(t *testing.T) {
+func TestParseStacks(t *testing.T) {
 	s := `    [D]    
 [N] [C]    
 [Z] [M] [P]
  1   2   3 
 `
-	crates := parseCrates(s)
+	stacks := parseStacks(s)
 
-	require.Len(t, crates, 3)
-	require.Equal(t, []rune{'Z', 'N'}, crates[1])
-	require.Equal(t, []rune{'M', 'C', 'D'}, crates[2])
-	require.Equal(t, []rune{'P'}, crates[3])
+	require.Len(t, stacks, 3)
+	require.Equal(t, []rune{'Z', 'N'}, stacks[1])
+	require.Equal(t, []rune{'M', 'C', 'D'}, stacks[2])
+	require.Equal(t, []rune{'P'}, stacks[3])
+}
+
+func TestParseStacks2(t *testing.T) {
+	s := `[T] [V]                     [W]    
+[V] [C] [P] [D]             [B]    
+[J] [P] [R] [N] [B]         [Z]    
+[W] [Q] [D] [M] [T]     [L] [T]    
+[N] [J] [H] [B] [P] [T] [P] [L]    
+[R] [D] [F] [P] [R] [P] [R] [S] [G]
+[M] [W] [J] [R] [V] [B] [J] [C] [S]
+[S] [B] [B] [F] [H] [C] [B] [N] [L]
+ 1   2   3   4   5   6   7   8   9 `
+	stacks := parseStacks(s)
+
+	require.Len(t, stacks, 9)
+	require.Equal(t, []rune{'C', 'B', 'P', 'T'}, stacks[6])
+	require.Equal(t, []rune{'L', 'S', 'G'}, stacks[9])
 }
 
 func TestParseInputWorks(t *testing.T) {
@@ -44,8 +61,18 @@ func TestParseInputWorks(t *testing.T) {
 	require.Len(t, instructions, 4)
 }
 
-// func TestPart2(t *testing.T) {
-// 	_, pairs := solve("my_test_input")
+func TestMovingCrates(t *testing.T) {
+	crates, instructions := parseInput("my_test_input")
 
-// 	require.Equal(t, 4, pairs)
-// }
+	moveCrates(crates, instructions)
+
+	require.Equal(t, []rune{'C'}, crates[1])
+	require.Equal(t, []rune{'M'}, crates[2])
+	require.Equal(t, []rune{'P', 'D', 'N', 'Z'}, crates[3])
+}
+
+func TestPart2(t *testing.T) {
+	topCrates := solve("my_test_input", moveCrates9001)
+
+	require.Equal(t, "MCD", topCrates)
+}
