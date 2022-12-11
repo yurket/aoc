378d2e4e60f2055c00ed035e70524d801207619d
yurket <yurket2006@yandex.ru>
2022-12-11 11:26:00 -0800

    Day9 part1

---
+++ day09/day09.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readLines(filename string) []string {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(content), "\n")
+	for lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1]
+	}
+	return lines
+}
+
+type Field [500][500]bool
+
+func parseMove(move string) (Point, int) {
+	t := strings.Split(move, " ")
+	steps, err := strconv.Atoi(t[1])
+	if err != nil {
+		panic(err)
+	}
+
+	var direction Point
+	switch t[0] {
+	case "L":
+		direction = Point{-1, 0}
+	case "R":
+		direction = Point{+1, 0}
+	case "U":
+		direction = Point{0, +1}
+	case "D":
+		direction = Point{0, -1}
+	default:
+		panic("Unknown direction")
+	}
+
+	return direction, steps
+}
+
+type Point struct {
+	x int
+	y int
+}
+
+func getMovement(diff int) int {
+	if diff > 0 {
+		return 1
+	} else if diff < 0 {
+		return -1
+	}
+	return 0
+}
+
+func (r *Point) Follow(head Point) {
+	dist := math.Sqrt(math.Pow(float64(head.x-r.x), 2) + math.Pow(float64(head.y-r.y), 2))
+	if dist < 2 {
+		return
+	}
+
+	xDiff, yDiff := head.x-r.x, head.y-r.y
+	r.x += getMovement(xDiff)
+	r.y += getMovement(yDiff)
+}
+
+func (r *Point) Add(p Point) {
+	r.x += p.x
+	r.y += p.y
+}
+
+func executeMoves(field *Field, moves []string) {
+	mid := int(len(field[0]) / 2)
+	head, tail := Point{mid, mid}, Point{mid, mid}
+	for _, move := range moves {
+		direction, steps := parseMove(move)
+		// fmt.Printf("Head %#v, Tail %#v.   Will move %s. \n", head, tail, move)
+		for i := steps; i > 0; i-- {
+			head.Add(direction)
+			tail.Follow(head)
+			field[tail.x][tail.y] = true
+		}
+	}
+}
+
+func countVisited(field Field) int {
+	visited := 0
+	for _, row := range field {
+		for _, x := range row {
+			if x {
+				visited++
+			}
+		}
+	}
+	return visited
+}
+
+func solve(filename string) (int, int) {
+	moves := readLines(filename)
+	field := Field{}
+	executeMoves(&field, moves)
+	visited := countVisited(field)
+
+	fmt.Printf("[Part 1] visited: %#v\n", visited)
+
+	// fmt.Printf("[Part 2] scenic Score: %d\n", scenicScore)
+
+	return visited, 0
+}
+
+func main() {
+	solve("input")
+}
---
+++ day09/day09_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart1(t *testing.T) {
+	visited, _ := solve("my_test_input")
+
+	require.Equal(t, 13, visited)
+}
+
+func TestRopeFollowDontMoves(t *testing.T) {
+	head, tail := Point{5, 5}, Point{5, 5}
+	tail.Follow(head)
+	require.Equal(t, head, tail)
+}
+
+func TestRopeFollowMovingStraight(t *testing.T) {
+	head, tail := Point{5, 5}, Point{3, 5}
+	tail.Follow(head)
+
+	require.Equal(t, Point{4, 5}, tail)
+}
+
+func TestRopeFollowMovingDiagonally(t *testing.T) {
+	head, tail := Point{0, 2}, Point{1, 0}
+	tail.Follow(head)
+	require.Equal(t, Point{0, 1}, tail)
+
+	head, tail = Point{1, 0}, Point{0, 2}
+	tail.Follow(head)
+	require.Equal(t, Point{1, 1}, tail)
+
+	head, tail = Point{1, 2}, Point{0, 0}
+	tail.Follow(head)
+	require.Equal(t, Point{1, 1}, tail)
+
+	head, tail = Point{0, 0}, Point{1, 2}
+	tail.Follow(head)
+	require.Equal(t, Point{0, 1}, tail)
+
+}
+
+// func TestPart2(t *testing.T) {
+// 	_, scenicScore := solve("my_test_input")
+
+// 	require.Equal(t, 8, scenicScore)
+// }
