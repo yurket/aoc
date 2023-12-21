c2dba1bff18a592a40c7bfaf09f68f027424cadb
yurket <yurket2006@yandex.ru>
2023-12-20 22:26:24 -0800

    Day15 part2

--- 2023/day15/day15.go
+++ 2023/day15/day15.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/yurket/aoc/util"
 )
 
-type Step []rune
+type Step struct {
+	step        []rune
+	label       string
+	operation   rune
+	focalLength int
+}
+
+type Lens struct {
+	label       string
+	focalLength int
+}
+
+type Boxes map[int][]Lens
+
+func print(bs Boxes) {
+	for boxNum, lenses := range bs {
+		fmt.Printf("Box %d: %+v\n", boxNum, lenses)
+	}
+}
 
 func readSteps(filename string) []Step {
 	lines := util.ReadLines(filename)
@@ -18,15 +38,33 @@ func readSteps(filename string) []Step {
 	input := lines[0]
 	stepStrings := strings.Split(input, ",")
 	steps := []Step{}
-	for _, step := range stepStrings {
-		steps = append(steps, []rune(step))
+	for _, stepS := range stepStrings {
+		step := Step{step: []rune(stepS)}
+		opIndex := strings.Index(stepS, "-")
+		if opIndex == -1 {
+			opIndex = strings.Index(stepS, "=")
+		}
+		step.operation = rune(stepS[opIndex])
+		step.label = stepS[:opIndex]
+
+		if opIndex+1 == len(stepS) {
+			step.focalLength = -1
+		} else {
+			focalLength, err := strconv.Atoi(stepS[opIndex+1:])
+			if err != nil {
+				panic(err)
+			}
+			step.focalLength = focalLength
+		}
+
+		steps = append(steps, step)
 	}
 	return steps
 }
 
-func HASH(step Step) int {
+func HASH(s []rune) int {
 	value := 0
-	for _, r := range step {
+	for _, r := range s {
 		value += int(r)
 		value = (value * 17) % 256
 	}
@@ -36,13 +74,44 @@ func HASH(step Step) int {
 func solve1(steps []Step) int {
 	sum := 0
 	for _, step := range steps {
-		sum += HASH(step)
+		sum += HASH(step.step)
 	}
 	return sum
 }
-func solve2(steps []Step) int {
 
-	return 0
+func HASHMAP(steps []Step) Boxes {
+	boxes := Boxes{}
+	for _, step := range steps {
+		boxNum := HASH([]rune(step.label))
+		if step.operation == '-' {
+			boxes[boxNum] = slices.DeleteFunc(boxes[boxNum], func(l Lens) bool { return l.label == step.label })
+		} else if step.operation == '=' {
+			lensIndex := slices.IndexFunc(boxes[boxNum], func(l Lens) bool { return l.label == step.label })
+			if lensIndex != -1 {
+				boxes[boxNum][lensIndex].focalLength = step.focalLength
+			} else {
+				boxes[boxNum] = append(boxes[boxNum], Lens{step.label, step.focalLength})
+			}
+		} else {
+			panic("Unknow operation!")
+		}
+		// fmt.Printf("\nStep '%s':\n", string(step.step))
+		// print(boxes)
+	}
+	return boxes
+}
+
+func solve2(steps []Step) int {
+	boxes := HASHMAP(steps)
+	focusingPower := 0
+	for boxNum, lenses := range boxes {
+		for slotNum, lens := range lenses {
+			power := (1 + boxNum) * (slotNum + 1) * lens.focalLength
+			// fmt.Printf("lens %+v: %d\n", lens, power)
+			focusingPower += power
+		}
+	}
+	return focusingPower
 }
 
 func main() {
--- 2023/day15/day15_test.go
+++ 2023/day15/day15_test.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestHash(t *testing.T) {
-	step := []rune("HASH")
-	require.Equal(t, 52, HASH(step))
+	step := Step{step: []rune("HASH")}
+	require.Equal(t, 52, HASH(step.step))
 }
 
 func TestSolve1(t *testing.T) {
 	steps := readSteps("test_input")
 
+	for _, s := range steps {
+		fmt.Printf("%+v\n", s)
+	}
+
 	require.Equal(t, 1320, solve1(steps))
 }
 
-// func TestSolve2(t *testing.T) {
-// 	lines := readLines("test_input")
+func TestHashmap(t *testing.T) {
+	steps := readSteps("test_input")
 
-// 	require.Equal(t, 2286, solve2(lines))
-// }
+	boxes := HASHMAP(steps)
+	exp1 := []Lens{{"rn", 1}, {"cm", 2}}
+	require.Equal(t, exp1, boxes[0])
+}
+
+func TestSolve2(t *testing.T) {
+	steps := readSteps("test_input")
+
+	require.Equal(t, 145, solve2(steps))
+}
