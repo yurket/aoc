c13a8556976237e02259e45dd24faf458305a7ee
yurket <yurket2006@yandex.ru>
2026-01-19 21:40:06 -0800

    Day 05 part 2

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{cmp::max, fs};
 
 type Range = (u128, u128);
 type Id = u128;
@@ -33,13 +33,41 @@ fn part1(ranges: &[Range], ids: &[u128]) -> i32 {
     fresh_count
 }
 
-fn merge_ranges(ranges: &mut [Range]) -> &mut [Range] {
+fn merge_ranges(orig_ranges: &[Range]) -> Vec<Range> {
+    let mut ranges = orig_ranges.to_vec();
+    if ranges.len() < 2 {
+        return ranges;
+    }
+
+    ranges.sort();
+    let mut i = 0;
+    let mut len = ranges.len();
+    loop {
+        if i + 1 > len - 1 {
+            break;
+        }
+        let current = &ranges[i];
+        let next = &ranges[i + 1];
+        if next.0 >= current.0 && next.0 <= current.1 {
+            ranges[i].1 = max(current.1, next.1);
+            ranges.remove(i + 1);
+            len -= 1;
+            continue;
+        }
+        i += 1;
+    }
+
     ranges
 }
 
-// fn part2(lines: &mut [Vec<char>]) -> i32 {
-//     0
-// }
+fn part2(ranges: &[Range]) -> u128 {
+    let merged_ranges = merge_ranges(ranges);
+
+    merged_ranges
+        .iter()
+        .map(|(start, end)| end - start + 1)
+        .sum()
+}
 
 fn main() {
     let (ranges, ids) = read_file("input");
@@ -49,8 +77,8 @@ fn main() {
     let sol1 = part1(&ranges, &ids);
     println!("Part 1 solution: {sol1}");
 
-    // let sol2 = part2(&mut lines);
-    // println!("Part 2 solution: {sol2}");
+    let sol2 = part2(&ranges);
+    println!("Part 2 solution: {sol2}");
 }
 
 #[cfg(test)]
@@ -63,9 +91,9 @@ mod tests {
         assert_eq!(part1(&ranges, &ids), 3);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     let mut lines = read_file("test_input");
-    //     assert_eq!(part2(&mut lines), 43);
-    // }
+    #[test]
+    fn test_part2() {
+        let (ranges, _) = read_file("test_input");
+        assert_eq!(part2(&ranges), 14);
+    }
 }
